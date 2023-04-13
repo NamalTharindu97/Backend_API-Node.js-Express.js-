80cc22dd81f86de0099d93cb97f33edec3895586
NamalTharindu97 <NamalA701997@gmail.com>
2023-04-13 21:45:27 +0530

    contacts routes file

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const dotenv = require("dotenv").config();
 
 const PORT = process.env.PORT || 5000;
 
+app.use("/api/contacts", require("./routes/contactRoutes"));
+
 app.listen(PORT, () => {
   console.log(`server running on port ${PORT}`);
 });
