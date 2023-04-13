c186b8710087cb45610fb506d50d8aad1cfa3dee
NamalTharindu97 <NamalA701997@gmail.com>
2023-04-13 22:37:54 +0530

    end points

--- routes/contactRoutes.js
+++ routes/contactRoutes.js
@@ -4,5 +4,17 @@ const router = express.Router();
 router.route("/").get((req, res) => {
   res.status(200).json({ message: "get all contacts" });
 });
+router.route("/").post((req, res) => {
+  res.status(200).json({ message: "create new contact" });
+});
+router.route("/:id").get((req, res) => {
+  res.status(200).json({ message: `get contact ${req.params.id}` });
+});
+router.route("/:id").put((req, res) => {
+  res.status(200).json({ message: `updatet contact ${req.params.id}` });
+});
+router.route("/:id").delete((req, res) => {
+  res.status(200).json({ message: `delete contact ${req.params.id}` });
+});
 
 module.exports = router;
