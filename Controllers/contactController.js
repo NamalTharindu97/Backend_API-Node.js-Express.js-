83df146c4ad556b73d58e24b93846d4632ee1a28
NamalTharindu97 <NamalA701997@gmail.com>
2023-04-14 20:43:29 +0530

    req body check

--- Controllers/contactController.js
+++ Controllers/contactController.js
@@ -9,6 +9,7 @@ const getContact = (req, res) => {
 //@Route POST api/contacts
 //@access public
 const postContact = (req, res) => {
+  console.log("this is the body", req.body);
   res.status(200).json({ message: "create new contact" });
 };
 
