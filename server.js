57e0788044becd4e290769a3ce1a3b6e31f55fe2
NamalTharindu97 <NamalA701997@gmail.com>
2023-04-15 16:44:08 +0530

    db connection

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const errorHandler = require("./middleware/errHandler");
+const dbConnection = require("./config/dbConnection");
 const app = express();
 const dotenv = require("dotenv").config();
 
+dbConnection();
 const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
