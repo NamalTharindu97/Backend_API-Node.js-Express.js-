2d7484f999936323d372ba97f4a27694bb09f2ac
NamalTharindu97 <NamalA701997@gmail.com>
2023-04-15 16:04:20 +0530

    express async handler

--- Controllers/contactController.js
+++ Controllers/contactController.js
@@ -1,14 +1,16 @@
+const asyncHandler = require("express-async-handler");
+
 //@desc GET all contacts
 //@Route GET api/contacts
 //@access public
-const getContact = (req, res) => {
+const getContact = asyncHandler(async (req, res) => {
   res.status(200).json({ message: "get all contacts" });
-};
+});
 
 //@desc POST contacts
 //@Route POST api/contacts
 //@access public
-const postContact = (req, res) => {
+const postContact = asyncHandler(async (req, res) => {
   console.log("this is the body", req.body);
   const { name, email, phone } = req.body;
   if (!name || !email || !phone) {
@@ -16,28 +18,28 @@ const postContact = (req, res) => {
     throw new Error("all fields are madotory");
   }
   res.status(200).json({ message: "create new contact" });
-};
+});
 
 //@desc GET one contact
 //@Route GET api/contacts/1
 //@access public
-const getOneContact = (req, res) => {
+const getOneContact = asyncHandler(async (req, res) => {
   res.status(200).json({ message: `get contact ${req.params.id}` });
-};
+});
 
 //@desc PUT one contact
 //@Route PUT api/contacts/1
 //@access public
-const updateContact = (req, res) => {
+const updateContact = asyncHandler(async (req, res) => {
   res.status(200).json({ message: `updatet contact ${req.params.id}` });
-};
+});
 
 //@desc GET one contact
 //@Route GET api/contacts/1
 //@access public
-const deleteContact = (req, res) => {
+const deleteContact = asyncHandler(async (req, res) => {
   res.status(200).json({ message: `delete contact ${req.params.id}` });
-};
+});
 
 module.exports = {
   getContact,
